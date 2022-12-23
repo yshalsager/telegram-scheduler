26cdfe0d6bb85f8983a7984221ae2d6e6fc42ca3
yshalsager <ysh-alsager@hotmail.com>
2022-12-23 21:33:58 +0200

    refactor: Use svelte bind in files form

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,26 +2,27 @@
 	import Home from '../components/Home.svelte';
 	import { splitLines } from '../lib/files.js';
 
-	let file;
-	let text;
+	let files;
+	let selectedFile;
+	let textContent;
 
-	async function readText(event) {
+	async function readText() {
 		// HTML <input> element returns a FileList.
 		// https://developer.mozilla.org/en-US/docs/Web/API/FileList
 		// We get the first selected item of the FileList, which is a File object.
 		// https://developer.mozilla.org/en-US/docs/Web/API/File
-		file = event.target.files.item(0);
+		selectedFile = files.item(0);
 		// The File object is a Blob, which has `text()` method that returns a Promise
 		//  that resolves with a string containing the contents of the blob, interpreted as UTF-8.
 		// https://developer.mozilla.org/en-US/docs/Web/API/Blob/text
-		const fileText = await file.text();
-		const splittedText = await splitLines(fileText);
-		text = splittedText.join('\n\n\n');
+		const selectedFileContent = await selectedFile.text();
+		const splittedText = await splitLines(selectedFileContent);
+		textContent = splittedText.join('\n\n\n');
 	}
 </script>
 
 <Home />
 
-<!-- <input type="file" on:change={readText} />
-<p>file: {file ? file.name : ''}</p>
-<pre id="output">{text ? text : ''}</pre> -->
+<input bind:files type="file" on:change={readText} />
+<p>file: {selectedFile ? selectedFile.name : ''}</p>
+<textarea cols="50" rows="5" bind:value={textContent} />
