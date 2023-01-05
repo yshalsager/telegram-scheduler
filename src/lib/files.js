b2dce02a2911e9a6c53092e3395f32f19f60ac04
yshalsager <ysh-alsager@hotmail.com>
2023-01-05 09:15:27 +0200

    fix(files): Handle last pattern occurrence

--- src/lib/files.js
+++ src/lib/files.js
@@ -8,8 +8,18 @@ export async function readAndSplitText(files, lineSeparator) {
 	//  that resolves with a string containing the contents of the blob, interpreted as UTF-8.
 	// https://developer.mozilla.org/en-US/docs/Web/API/Blob/text
 	const selectedFileContent = await selectedFile.text();
-	const splittedText =
+	let splittedText =
 		selectedFileContent.match(new RegExp(`^([\\s\\S]+?)(?=\\s+${lineSeparator})`, 'gm')) || [];
+	// Last match isn't included, let's add it.
+	const lastOccurrence = splittedText[splittedText.length - 1];
+	const remainingText = selectedFileContent.slice(
+		selectedFileContent.indexOf(lastOccurrence) + lastOccurrence.length + 1
+	);
+	const remainingMatch = remainingText.match(
+		new RegExp(`^([\\s\\S]+)(?!\\s+${lineSeparator})`, 'gm')
+	);
+	splittedText = splittedText.concat(remainingMatch);
+	// console.log(splittedText);
 	return splittedText.map((text, index) => {
 		return { id: index, text: text, prefix: '', suffix: '' };
 	});
