648b2d194a6823ef18f69584f62bad151ef9b6e0
yshalsager <ysh-alsager@hotmail.com>
2023-01-18 22:52:59 +0200

    refactor: Improve splitting message file and drop using regex

--- src/lib/files.js
+++ src/lib/files.js
@@ -1,3 +1,11 @@
+/**
+ *
+ * @param {FileList} files - Files list from html input to take the first file to be read and split
+ * @param {string} lineSeparator - The separator used to split the file's content
+ *
+ * @returns {Array} - An array of objects, where each object has the properties 'id', 'text', 'prefix', and 'suffix'
+ */
+
 export async function readAndSplitText(files, lineSeparator) {
 	// HTML <input> element returns a FileList.
 	// https://developer.mozilla.org/en-US/docs/Web/API/FileList
@@ -8,11 +16,29 @@ export async function readAndSplitText(files, lineSeparator) {
 	//  that resolves with a string containing the contents of the blob, interpreted as UTF-8.
 	// https://developer.mozilla.org/en-US/docs/Web/API/Blob/text
 	const selectedFileContent = await selectedFile.text();
-	let splittedText =
-		selectedFileContent.split(new RegExp(`^([\\s\\S]+?)(?=\\s+${lineSeparator})`, 'gm')) || [];
-	return splittedText
-		.filter((line) => line !== '\n' && line !== '')
-		.map((text, index) => {
-			return { id: index, text: text.trim(), prefix: '', suffix: '' };
-		});
+	let splittedText = [];
+	let noteIndex = 0;
+	const lines = selectedFileContent.split('\n');
+	let text;
+
+	lines.forEach((line, itemIndex) => {
+		if (line.startsWith(lineSeparator)) {
+			// Case 1: First match
+			if (!text) {
+				text = line.trim();
+			} else {
+				// Case 2: Any other match
+				splittedText.push({ id: noteIndex++, text, prefix: '', suffix: '' });
+				text = line.trim();
+			}
+		} else {
+			if (text) text += '\n' + line;
+		}
+		if (itemIndex === lines.length - 1) {
+			// Last line
+			text += '\n' + line;
+			splittedText.push({ id: noteIndex++, text, prefix: '', suffix: '' });
+		}
+	});
+	return splittedText;
 }
