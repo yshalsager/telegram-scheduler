db8d6d0e688986d1346f7bc9c1bca5b0dbb80b2e
yshalsager <ysh-alsager@hotmail.com>
2025-03-22 14:40:11 +0300

    feat(theme): add switchTheme function to change themes

--- src/lib/theme.js
+++ src/lib/theme.js
@@ -1,5 +1,8 @@
 import {theme} from '../store.js'
 
+export const switchTheme = (newTheme) => {
+    theme.set(newTheme)
+}
 
 export const toggleTheme = (currentTheme) => {
     const newTheme = currentTheme === 'telegram' ? 'telegramDark' : 'telegram'
